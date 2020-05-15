2ee882e49493cc7229780a7155da84142fe86b65
Prithvi0 <prithviy96@gmail.com>
2020-05-15 07:46:30 +0530

    Modified database file by including saltRounds, jwtToken & expireTime

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -1,3 +1,6 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/node-demo'
+    url: 'mongodb://localhost:27017/node-demo',
+    saltRounds: 2,
+    jwtSecret: 'yo-its-a-secret',
+    tokenExpireTime: '6h'
 }
