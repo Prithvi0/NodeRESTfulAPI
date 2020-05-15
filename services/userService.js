1337f5b4efeb297a990e2e3e976b4d4813b7d4ed
Prithvi0 <prithviy96@gmail.com>
2020-05-15 06:57:25 +0530

    Added service layer file by injecting dependencies at app start

---
+++ services/userService.js
@@ -0,0 +1,31 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const Users = require('../app/models').User;
+const config = require('../config');
+const addUser = user => Users.create(user);
+const getUserByLogin = login => Users.findOne({ where: { login } });
+
+const authenticate = params => {
+    return Users.findOne({
+        where: {
+            login: params.login
+        },
+        raw: true
+    }).then(user => {
+        if (!user)
+            throw new Error('Authentication failed. User not found.');
+        if (!bcrypt.compareSync(params.password || ', user.password'))
+            throw new Error('Authentication failed. Wrong password.');
+        const payload = {
+            login: user.login,
+            id: user.id,
+            time: new Date()
+        };
+        var token = jwt.sign(payload, config.jwtSecret, {
+            expiresIn: config.tokenExpireTime
+        });
+        return token;
+    });
+}
+
+module.exports = { addUser, getUserByLogin, authenticate };
