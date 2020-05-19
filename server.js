8f180d41ca64ca5623bbcf45c69eff1b2d6162bc
Prithvi0 <prithviy96@gmail.com>
2020-05-19 07:46:30 +0530

    Modified server file by removing the database connection code

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const mongoose = require('mongoose');
 const http = require('http');
 const port = process.env.PORT || 7001;
 const server = http.createServer(app);
@@ -11,18 +10,6 @@ dotenv.config();
 // parse requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// connecting to the database
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-}).then(() => {
-    console.log("Successfully connected to the database");    
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-});
 
 // route definition
 app.get('/', (request, response) => {
