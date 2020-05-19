7ef52f7cdf022df5bbff997030db2cef0f002f02
Prithvi0 <prithviy96@gmail.com>
2020-05-19 07:47:31 +0530

    Modified the config file by adding the database connection code

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -1,5 +1,21 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// connecting to the database
+module.exports = mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
 module.exports = {
     saltRounds: 2,
     jwtSecret: 'yo-its-a-secret',
-    tokenExpireTime: '6h'
+    tokenExpireTime: '6h',
 }
