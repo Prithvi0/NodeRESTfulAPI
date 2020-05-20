e8d81b4233e1f5da5c57265e488f7113338efcb7
Prithvi0 <prithviy96@gmail.com>
2020-05-20 08:02:24 +0530

    Modified the controller file by adding comments

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,80 +1,84 @@
+/** It passes request to login & register microservices & get response
+ * @param {httpRequest} req
+ * @param {httpresponse} res
+ * @param {callbackfunction} callback
+ */
+
 const userService = require('../services/userService');
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 
 exports.register = (req, res) => {
-    userService.find({ 'emailId': req.body.emailId })
-        .exec()
-        .then(user => {
-            if (user.length >= 1) {
-                return res.status(409).json({
-                    message: "E-Mail exists"
-                });
-            } else {
-                bcrypt.hashSync(req.body.password, config.saltRounds, (err, hash) => {
-                    if (err) {
-                        return res.status(500).json({
-                            error: err
-                        });
-                    } else {
-                        const user = new User({
-                            'userName': req.body.userName,
-                            'emailId': req.body.emailId,
-                            'password': hash
-                        });
-                        user.save()
-                            .then(result => {
-                                console.log(result);
-                                res.status(201).json({
-                                    message: "User created"
-                                });
-                            }).catch(err => {
-                                console.log(err);
-                                res.status(500).json({
-                                    error: err
-                                });
-                            });
-                    }
-                });
-            }
+  userService.find({ 'emailId': req.body.emailId })
+    .exec()
+    .then(user => {
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: "E-Mail exists"
         });
+      } else {
+        bcrypt.hashSync(req.body.password, config.saltRounds, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err
+            });
+          } else {
+            const user = new User({
+              'userName': req.body.userName,
+              'emailId': req.body.emailId,
+              'password': hash
+            });
+            user.save()
+              .then(result => {
+                console.log(result);
+                res.status(201).json({
+                  message: "User created"
+                });
+              }).catch(err => {
+                console.log(err);
+                res.status(500).json({
+                  error: err
+                });
+              });
+          }
+        });
+      }
+    });
 };
 
 exports.login = (req, res) => {
-    User.find({ 'emailId': req.body.emailId })
-        .exec()
-        .then(user => {
-            if (user.length < 1) {
-                return res.status(401).json({
-                    message: "Authentication failed"
-                });
-            }
-            bcrypt.compare(req.body.password, user.password, (err, result) => {
-                if (err) {
-                    return res.status(401).json({
-                        message: "Authentication failed"
-                    });
-                }
-                if (result) {
-                    const token = jwt.sign(
-                        {
-                            'emailId': user.emailId,
-                            'password': user.password
-                        },
-                    );
-                    return res.status(200).json({
-                        message: "Authentication successful",
-                        token: token
-                    });
-                }
-                res.status(401).json({
-                    message: "Authentication failed"
-                });
-            });
-        }).catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
+  User.find({ 'emailId': req.body.emailId })
+    .exec()
+    .then(user => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Authentication failed"
         });
+      }
+      bcrypt.compare(req.body.password, user.password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Authentication failed"
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              'emailId': user.emailId,
+              'password': user.password
+            },
+          );
+          return res.status(200).json({
+            message: "Authentication successful",
+            token: token
+          });
+        }
+        res.status(401).json({
+          message: "Authentication failed"
+        });
+      });
+    }).catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 };
